0f7a314835c1c1a246c6732954212923cd5b48fd
JaimieWalker <jaimie.walker90@gmail.com>
2016-12-05 12:24:56 -0500

    commit

---
+++ app/assets/javascripts/zombie.js
@@ -0,0 +1,16 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+var game = new Phaser.Game(800,600, Phaser.AUTO,'',{preload: preload, create: create, update: update});
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+	
+}
